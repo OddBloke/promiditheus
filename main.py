5b7e4dbb71f4185bf521e6297ec0be1abe5f3f20
Daniel Watkins <daniel@daniel-watkins.co.uk>
2021-05-14 00:39:36 -0400

    lift logic into QueryPlayer class

--- main.py
+++ main.py
@@ -38,56 +38,67 @@ INSTRUMENTS = {
 }
 
 
-def get_value() -> float:
-    json = requests.get(
-        QUERY_TEMPLATE.format(query=QUERY.format(instance="<redacted>:9100"))
-    ).json()
-    logging.info("Prometheus JSON: %s", json)
-    timestamp, value = json["data"]["result"][0]["value"]
-    logging.info("Metric value: %s", value)
-    return float(value)
+class QueryPlayer:
+    def __init__(self, name: str, instrument: Instrument, query: str):
+        self._name = name
+        self._instrument = instrument
+        self._query = query
 
+        self._last_note = None
 
-def handle_value(port: mido.ports.BaseOutput, value: float, last_note: int) -> None:
-    note = INSTRUMENTS["cello"].clamp(value)
-    logging.info("Note: %s", note)
+        logging.info("Opening MIDI output: %s", MIDI_OUTPUT_NAME)
+        self._port = mido.open_output(MIDI_OUTPUT_NAME)
 
-    if note != last_note:
-        if last_note is not None:
-            port.send(mido.Message("note_off", note=last_note))
-        port.send(mido.Message("note_on", note=note, velocity=127))
-    return note
+    def _get_value(self) -> float:
+        json = requests.get(
+            QUERY_TEMPLATE.format(query=self._query.format(instance="<redacted>:9100"))
+        ).json()
+        logging.info("Prometheus JSON: %s", json)
+        timestamp, value = json["data"]["result"][0]["value"]
+        logging.info("Metric value: %s", value)
+        return float(value)
+
+    def _handle_value(self, value: float) -> None:
+        note = self._instrument.clamp(value)
+        logging.info("Note: %s", note)
+
+        if note != self._last_note:
+            self.off()
+            self._port.send(mido.Message("note_on", note=note, velocity=127))
+        self._last_note = note
+        return note
+
+    def off(self):
+        if self._last_note is not None:
+            self._port.send(mido.Message("note_off", note=self._last_note))
+
+    def tick(self):
+        self._handle_value(self._get_value())
 
 
 def main():
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s %(levelname)-8s %(message)s',
+        format="%(asctime)s %(levelname)-8s %(message)s",
     )
-
-    logging.info("Opening MIDI output: %s", MIDI_OUTPUT_NAME)
-    port = mido.open_output(MIDI_OUTPUT_NAME)
+    player = QueryPlayer("CPU", INSTRUMENTS["cello"], QUERY)
 
     try:
-        last_note = None
         while True:
             logging.info("Starting loop...")
             start = time.time()
             try:
-                value = get_value()
+                player.tick()
             except (IndexError, requests.exceptions.ConnectionError) as exc:
                 # Ignore these errors by falling through to the sleep logic
                 logging.exception("Ignoring occasional error")
-            else:
-                last_note = handle_value(port, value, last_note)
 
             # Attempt to reduce drift
             delta = (start + 5) - time.time()
             logging.info("Loop complete; sleeping for %ss", delta)
             time.sleep(delta)
     finally:
-        if last_note is not None:
-            port.send(mido.Message("note_off", note=last_note))
+        player.off()
 
 
 if __name__ == "__main__":
