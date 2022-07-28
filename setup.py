7433a7b737c2482d19078cb84b7d466d3f82fd0d
Daniel Watkins <daniel@daniel-watkins.co.uk>
2022-07-28 08:51:37 -0400

    make pip-installable

---
+++ setup.py
@@ -0,0 +1,6 @@
+# This file is required for older versions of pip to `install --editable`; see
+# the Tip under
+# https://setuptools.pypa.io/en/latest/userguide/quickstart.html#development-mode
+from setuptools import setup
+
+setup()
